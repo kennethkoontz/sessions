7e94bcfbba4d03db3bb9d2aef1aa93e52044300b
ken koontz <kenneth.koontz@gmail.com>
2016-01-06 23:45:25 -0800

    sessions

--- build/app/app.module.js
+++ build/app/app.module.js
@@ -4,75 +4,84 @@ angular
   ])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('session', {
+      .state('main', {
+        templateUrl: '/session.html'
+      })
+      .state('main.session', {
         url: "/session/:id",
-        controller: "SessionCtrl",
-        templateUrl: "/session.html",
-        resolve: {
-          blocks: function ($http, $stateParams) {
-            return $http({
-              method: 'get',
-              url: '/blocks',
-              params: {
-                session: $stateParams.id
+        views: {
+          sessions: {
+            controller: "SessionsCtrl",
+            templateUrl: "sessions.html",
+            resolve: {
+              sessions: function ($http) {
+                return $http({
+                  method: 'get',
+                  url: '/sessions'
+                }).then(function (res) {
+                  return res.data;
+                });
               }
-            }).then(function (res) {
-              return res.data;
-            });
+            }
           },
-          session: function ($http, $stateParams) {
-            return $http({
-              method: 'get',
-              url: '/sessions/' + $stateParams.id
-            }).then(function (res) {
-              return res.data;
-            });
+          members: {
+            controller: "MembersCtrl",
+            templateUrl: "members.html",
+            resolve: {
+              session: function ($http, $stateParams) {
+                return $http({
+                  method: 'get',
+                  url: '/sessions/' + $stateParams.id
+                }).then(function (res) {
+                  return res.data;
+                });
+              }
+            }
           },
-          sessions: function ($http) {
-            return $http({
-              method: 'get',
-              url: '/sessions'
-            }).then(function (res) {
-              return res.data;
-            });
+          blocks: {
+            controller: "BlocksCtrl",
+            templateUrl: '/blocks.html',
+            resolve: {
+              blocks: function ($http, $stateParams) {
+                return $http({
+                  method: 'get',
+                  url: '/blocks',
+                  params: {
+                    session: $stateParams.id
+                  }
+                }).then(function (res) {
+                  return res.data;
+                });
+              },
+              session: function ($http, $stateParams) {
+                return $http({
+                  method: 'get',
+                  url: '/sessions/' + $stateParams.id
+                }).then(function (res) {
+                  return res.data;
+                });
+              },
+              sessions: function ($http) {
+                return $http({
+                  method: 'get',
+                  url: '/sessions'
+                }).then(function (res) {
+                  return res.data;
+                });
+              }
+            },
           }
         }
       });
     $urlRouterProvider.otherwise('/session/');
   })
-  .controller('SessionCtrl', function ($scope, $http, $state, $stateParams, sessions, session, blocks) {
-    if (!$stateParams.id && sessions.length) {
-      $state.go('session', {id: sessions[0]._id});
-      return;
-    }
+  .controller('BlocksCtrl', function ($scope, $http, $stateParams, $state, blocks, session, sessions) {
+    if ($stateParams.id == '' && sessions.length > 0)
+      $state.go('main.session', {id: sessions[0]._id});
+
     socket.emit('enter-room', {session_id: $stateParams.id, user_id: '566f54028750c50d3fb227f1'});
-    $scope.newSession = {};
-    $scope.sessions = sessions;
     $scope.session = session;
     $scope.blocks = blocks;
-
-    $scope.activateNewSession = function () {
-      $scope.newSession.active = true;
-    };
-
-    $scope.createSession = function (e) {
-      if (e.keyCode === 13) {
-        $http({
-          method: 'post',
-          url: '/sessions',
-          data: $scope.newSession
-        })
-          .then(function (res) {
-            $scope.sessions.push(res.data);
-            $scope.newSession = {};
-          });
-      }
-    };
-
-    $scope.shareSessionLink = function () {
-      $scope.link = $scope.session.link;
-    };
-
     $scope.createBlock = function (session) {
       $http({
         url: '/blocks',
@@ -107,5 +116,34 @@ angular
       }).then(function (res) {
         block.status = res.data.status
       });
+    };
+  })
+  .controller('MembersCtrl', function ($scope, session) {
+    $scope.session = session;
+  })
+  .controller('SessionsCtrl', function ($scope, $http, $state, $stateParams, sessions) {
+    $scope.newSession = {};
+    $scope.sessions = sessions;
+
+    $scope.activateNewSession = function () {
+      $scope.newSession.active = true;
+    };
+
+    $scope.createSession = function (e) {
+      if (e.keyCode === 13) {
+        $http({
+          method: 'post',
+          url: '/sessions',
+          data: $scope.newSession
+        })
+          .then(function (res) {
+            $scope.sessions.push(res.data);
+            $scope.newSession = {};
+          });
+      }
+    };
+
+    $scope.shareSessionLink = function () {
+      $scope.link = $scope.session.link;
     };
   });
